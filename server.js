38223224b4c5d1a86f583bd331f62834d5c58f56
Swanand01 <75439077+Swanand01@users.noreply.github.com>
2022-12-04 01:35:12 +0530

    dotenv require changes

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
+}
 
 const appID = process.env.APP_ID
 const appCertificate = process.env.APP_CERTIFICATE
